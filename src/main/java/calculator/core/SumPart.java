a6672b3d8c82f4a22e73ef36b247aac36f1cf71b
harameter <haram1000068@gmail.com>
2025-10-20 23:21:50 +0900

    feat(SumPart.java):  sp_empty 삭제
    덧셈구간이 비었는지 판별하는 메서드였으나 다른 클래스 구현 후 필요가 없어 삭제

--- src/main/java/calculator/core/SumPart.java
+++ src/main/java/calculator/core/SumPart.java
@@ -7,10 +7,6 @@ public class SumPart {
 		this.value = v;
 	}
 
-	public static SumPart sp_Empty(String v, boolean isEmpty) {
-		return new SumPart("");
-	}
-
 	public static SumPart sp_notCustom(String v, boolean hasCustom) {
 		if (!hasCustom)	return new SumPart(v);
 		if ((v.length() >= 4) && (v.charAt(3) == '\n')) return new SumPart(v.substring(4));
