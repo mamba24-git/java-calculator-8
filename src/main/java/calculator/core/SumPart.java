e0854be61ce7f9f299744d5012563359c0ee94ef
harameter <haram1000068@gmail.com>
2025-10-20 21:26:54 +0900

    fix: 커스텀 구분자 존재시 계산 수행 부분 슬라이싱 오류 수정
    입력값에서 커스텀 구분자 지정 시 사용되는 문자(열)에 '\'+'n'과 개행문자 '\n' 을 둘 다 포함시키는 것으로 변경

--- src/main/java/calculator/core/SumPart.java
+++ src/main/java/calculator/core/SumPart.java
@@ -13,6 +13,13 @@ public class SumPart {
 
 	public static SumPart sp_notCustom(String v, boolean hasCustom) {
 		if (!hasCustom)	return new SumPart(v);
-		return new SumPart(v.substring(4));
+		if (v.length() >= 4 && v.charAt(3) == '\n') return new SumPart(v.substring(4));
+		if (v.length() >= 5 && v.charAt(3) == '\\' && v.charAt(4) == 'n') return new SumPart(v.substring(5));
+
+		throw new IllegalArgumentException("커스텀 구분자 선언 형식 오류");
+	}
+
+	public String raw() {
+		return value;
 	}
 }
