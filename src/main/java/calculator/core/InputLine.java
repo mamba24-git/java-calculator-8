e16c17a1d1f22d9411942c4a120e337103f8e386
harameter <haram1000068@gmail.com>
2025-10-20 21:25:38 +0900

    fix: 커스텀 구분자 여부 로직 오류 수정
    입력값에서 커스텀 구분자 지정 시 사용되는 문자(열)에 '\'+'n'과 개행문자 '\n' 을 둘 다 포함시키는 것으로 변경

--- src/main/java/calculator/core/InputLine.java
+++ src/main/java/calculator/core/InputLine.java
@@ -17,8 +17,9 @@ public final class InputLine {
 		if (value.length() < 4)	return false;
 		if (value.charAt(0) != '/')	return false;
 		if (value.charAt(1) != '/')	return false;
-		if (value.charAt(3) != '\n') return false;
-		return true;
+		if (value.charAt(3) == '\n') return true;
+		if (value.length() >= 5 && value.charAt(3) == '\\' && value.charAt(4) == 'n') return true;
+		return false;
 	}
 
 	public String raw() {
