c2ccc9440997fc6f0834ceaa0628d8abac826d20
harameter <haram1000068@gmail.com>
2025-10-20 22:00:43 +0900

    feat: 구현된 클래스 및 메서드를 이용해 프로그램 수행
    입력받은 문자열에 따라 예외 처리를 수행하거나 뎃셈 연산 수행 후 결과값 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,39 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import calculator.core.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.\n");
+        String raw = Console.readLine();
+
+        InputLine line = new InputLine(raw);
+
+        System.out.print("결과 : ");
+
+        if (line.isEmpty()) {
+            System.out.println(0);
+            return;
+        }
+
+        boolean hasCustom = line.hasCustom();
+        Delimiters ds = Delimiters.delims(line.raw(), hasCustom);
+        SumPart part = SumPart.sp_notCustom(line.raw(), hasCustom);
+
+        StringScanner sc = new StringScanner(ds);
+
+        int[] digits = sc.scan(part);
+
+        Sum sum = new Sum();
+        int i = 0;
+        while (i < digits.length) {
+            sum.sum_values(digits[i]);
+            i = i + 1;
+        }
+
+        System.out.println(sum.toInt());
     }
 }
